8c0df4018ae6665f8a7086d2e33a7e5f69b7ba16
Bharath <bharathbdh65@gmail.com>
2023-11-02 23:22:56 +0530

    Update routes.py

--- mainflask/routes.py
+++ mainflask/routes.py
@@ -7,7 +7,10 @@ import secrets
 import os
 from PIL import Image
 from mainflask.blinkdetect import gen_frames
-
+from sumy.parsers.html import HtmlParser
+from sumy.parsers.plaintext import PlaintextParser
+from sumy.nlp.tokenizers import Tokenizer
+from sumy.summarizers.lsa import LsaSummarizer
 
     
 posts=[
